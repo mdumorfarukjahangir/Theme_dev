bfc81bb10907e1c067310d2dd47f6c8fa99e487b
mdumorfarukjahangir <ufjahangir96@gmail.com>
2019-12-06 19:40:48 +0600

    Set a Previous post and Next post in a single page

--- single.php
+++ single.php
@@ -101,16 +101,34 @@ get_header();
             <div class="s-content__pagenav">
                 <div class="s-content__nav">
                     <div class="s-content__prev">
-                        <a href="#0" rel="prev">
-                            <span>Previous Post</span>
-                            Tips on Minimalist Design
+                    <?php 
+                      $philosophy_previous_post = get_previous_post();
+                      if($philosophy_previous_post):
+                    ?>
+                        <a href="<?php echo esc_url(get_the_permalink( $philosophy_previous_post )); ?>" rel="prev">
+                            <span><?php _e("Previous Post","philosophy"); ?></span>
+                            <?php 
+                              echo get_the_title($philosophy_previous_post);
+                            ?>
                         </a>
+                        <?php 
+                          endif;
+                        ?>
                     </div>
                     <div class="s-content__next">
-                        <a href="#0" rel="next">
-                            <span>Next Post</span>
-                            Less Is More
+                    <?php 
+                      $philosophy_next_post = get_next_post();
+                      if($philosophy_next_post):
+                    ?>
+                        <a href="<?php echo esc_url(get_the_permalink( $philosophy_next_post )); ?>" rel="prev">
+                            <span><?php _e("Next Post","philosophy"); ?></span>
+                            <?php 
+                              echo get_the_title($philosophy_next_post);
+                            ?>
                         </a>
+                        <?php 
+                          endif;
+                        ?>
                     </div>
                 </div>
             </div> <!-- end s-content__pagenav -->
