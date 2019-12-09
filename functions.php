ec3dd2597567327c11e1b36fab209f2c05d15a89
mdumorfarukjahangir <ufjahangir96@gmail.com>
2019-12-09 11:29:30 +0600

    Display all tags

--- functions.php
+++ functions.php
@@ -103,5 +103,16 @@ function philosophy_widget() {
         'before_title'  => '<h3>',
         'after_title'   => '</h3>',
     ) );
+
+    register_sidebar( array(
+        'name'          => __( 'Before footer section ', 'philosophy' ),
+        'id'            => 'before_footer_section',
+        'description'   => __( 'Before footer section ', 'philosophy' ),
+        'before_widget' => '<div class="%2$s">',
+        'after_widget'  => "</div>",
+        'before_title'  => '<h3>',
+        'after_title'   => '</h3>',
+    ) );
 }
 add_action( 'widgets_init', 'philosophy_widget' );
+
